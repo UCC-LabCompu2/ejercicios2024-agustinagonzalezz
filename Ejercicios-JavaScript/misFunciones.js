22b7227d83b0f6bf02842f9fc53c8e4d6fa03396
agus gonzalez <2313442@ucc.edu.ar>
2024-05-22 10:43:52 -0300

    agrega ej de GR

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -36,3 +36,23 @@ function convertirUnidades(nombre, valor){
         document.getElementById("metro").value = valor *0.9144;
     }
 }
+
+/**
+ * Permite convertir grados a radiantes
+ * @method convertirGR
+ * @param {String} id
+ */
+
+function convertirGR(id){
+    let grad, rad;
+    if(id === "grados"){
+        grad = document.getElementById("grados").value;
+        rad= grad * Math.PI / 180;
+        document.getElementById("radianes").value=rad;
+    }
+    else if (id==="radianes"){
+        rad = document.getElementById("radianes").value;
+        grad = rad * 180 / Math.PI;
+        document.getElementById("grados").value=grad;
+    }
+}
